4911eb9dadec66064e22acd9e912a479dfd62f9c
iMac <imac@Nileshs-iMac.local>
2024-07-01 20:49:32 +0530

    changes in main file

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 void main() {
+  // changes from t
   runApp(const MyApp());
 }
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Demo t',
         theme: ThemeData(
           // This is the theme of your application.
           //
@@ -60,6 +61,7 @@ class HomeController extends GetxController {
     SelectionModel(name: "7", selected: false, id: "7"),
     SelectionModel(name: "8", selected: false, id: "8"),
     SelectionModel(name: "9", selected: false, id: "9"),
+    SelectionModel(name: "10", selected: false, id: "10"),
   ]);
 
   RxList<String> ids = RxList([]);
